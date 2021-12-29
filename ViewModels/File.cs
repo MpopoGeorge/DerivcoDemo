3a8f54dbbfa8ec16b396ab9bf9b74c13bc5cbc71
George Mpopo <gtmpopo@gmail.com>
2021-12-29 14:31:12 +0200

    Database Script Added

---
+++ ViewModels/File.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace DerivcoDemo.ViewModels
+{
+    public class File
+    {
+        public int FileId { get; set; }
+        [StringLength(255)]
+        public string FileName { get; set; }
+        [StringLength(100)]
+        public string ContentType { get; set; }
+        public byte[] Content { get; set; }
+        public FileType FileType { get; set; }
+        public int PersonId { get; set; }
+        public virtual MenuItem MenuItem { get; set; }
+    }
+}
