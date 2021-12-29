7563544296435f1dafff3f66115b3031b52307e6
George Mpopo <gtmpopo@gmail.com>
2021-12-29 22:27:06 +0200

    Added Helpers folder for custom Helper Classes

--- ViewModels/MenuItemViewModel.cs
+++ ViewModels/MenuItemViewModel.cs
@@ -9,24 +9,39 @@ namespace DerivcoDemo.ViewModels
 {
     public class MenuItemViewModel
     {
+        [Display(Name = "Customer ID", AutoGenerateFilter = false)]
         public Guid MenuItemId { get; set; }
+
+
+        [Display(Name = "Category ID", AutoGenerateFilter = false)]
         public int CategoryId { get; set; }
+
+
+        [Display(Name = "Code", AutoGenerateFilter = false)]
 
         [Required]
         public string MenuItemCode { get; set; }
 
         [Required]
+        [Display(Name = "Name", AutoGenerateFilter = false)]
         public string MenuItemName { get; set; }
 
+        [Required]
+        [Display(Name = "Description", AutoGenerateFilter = false)]
         public string MenuItemDescription { get; set; }
 
+
         [Required]
+        [Display(Name = "Price", AutoGenerateFilter = false)]
         public decimal MenuItemPrice { get; set; }
-        
+
+
         [Required]
+        [Display(Name = "Thumbnail", AutoGenerateFilter = false)]
         public HttpPostedFileBase ItemImagePath { get; set; }
 
         [Required]
+        [Display(Name = "Category", AutoGenerateFilter = false)]
         public IEnumerable<SelectListItem> CategorySelectListItem { get; set; }
 
     }
